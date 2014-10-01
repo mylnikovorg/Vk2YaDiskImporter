a51bfeda8bfc9f2a594906f2be827c110a9e9907
root <root@localhost.localdomain>
2014-10-01 13:08:57 +0400

    first commit

---
+++ src/main/java/org/mylnikov/vk2yadisk/HelloController.java
@@ -0,0 +1,18 @@
+package org.mylnikov.vk2yadisk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+        VlClient vk = new VlClient();
+		model.addAttribute("message", vk.getWallDocsOfGroup(31513532));
+        //vk.getUserGroups();
+		return "hello";
+	}
+}
